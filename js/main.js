93843813b838c4dd687e4073645603f30acb466a
Rouslan Abdrakhmanov <root.of.system@gmail.com>
2020-12-12 23:13:33 +0300

    changed default color of net effect

--- js/main.js
+++ js/main.js
@@ -1,25 +1,37 @@
 (function (w, $) {
 
-    const effect = VANTA.NET({
+    const queryString = w.location.search;
+    const urlParams = new URLSearchParams(queryString);
+
+    /**
+     *   Green A700 - 0x00c853
+     *   Blue A700 - 0x1976d2
+     *   Light blue A700 - 0x0288d1
+     */
+
+    let effectOptions = {
         el: "#net",
         mouseControls: true,
         touchControls: true,
         gyroControls: false,
-        minHeight: 200.00,
-        minWidth: 200.00,
+        minHeight: 72.00,
+        minWidth: 128.00,
         scale: 1.00,
         scaleMobile: 1.00,
-        color: 0x00c853, //Green A700
+        color: 0x1976d2,
         backgroundColor: 0x212121, //Grey 900
         points: 10.00,
         maxDistance: 23.00,
         spacing: 15.00
-    });
+    }
+
+    const effect = VANTA.NET(effectOptions);
 
     $(w).on('resize', function () {
         console.log('win.resize', effect);
         var win_w = $(w).width(),
-            win_h = $(w).height();
+            win_h = $(w).height(),
+            square = win_w * win_h;
 
         $('#net').css({
             'width': win_w,
@@ -27,14 +39,14 @@
         });
 
         $('#ug-logo').css({
-            'width': win_w / 8,
-            'height': win_w / 8
-        });
+            'width': square / 10000,
+            'height': square / 10000
+        }).show();
 
         effect.resize();
 
         $('body').css({
-            'font-size': (win_w * win_h) / 140000 + 'px'
+            'font-size': square / 140000 + 'px'
         })
     });
 
